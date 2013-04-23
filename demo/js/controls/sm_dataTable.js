7410eaa69c5dc1963ad1a2cbec223a6a83648200
liuhong <525754781@qq.com>
2013-04-23 14:54:48 +0800

    datatables

---
+++ demo/js/controls/sm_dataTable.js
@@ -0,0 +1,51 @@
+﻿/**
+ * Class: SuperMap.Bev.DataTable
+ * 表格控件。
+ */
+(function () {
+    SuperMap.Bev.Class.create(
+        "SuperMap.Bev.DataTable",
+        {
+            /**
+             * APIProperty: body
+             * {HTMLElement} 父容器
+             */
+            body:null,
+            /**
+             * APIProperty: datas
+             * {Array<String>} 表格中要显示的数据
+             */
+            datas:[],
+            /**
+             * APIProperty: ColumnTitles
+             * {Array<String>} 每一列的标题
+             */
+            columnTitles:[],
+            /**
+             * Constructor: SuperMap.Bev.DataTable
+             * 实例化 DataTable 类。
+             *
+             * Parameters:
+             * options - {Object} 参数
+             *
+             * Examples:
+             * (code)
+             * (end)
+             */
+            init:function (options) {
+
+                this.create();
+            },
+            /**
+             * Method: create
+             * 创建该控件的dom对象。
+             */
+            create:function(){
+
+            }
+        },
+        null,                        //父类
+        false,                       //是否是静态类
+        null                         //初始化该类之前需要加载的js文件
+    );
+})();
